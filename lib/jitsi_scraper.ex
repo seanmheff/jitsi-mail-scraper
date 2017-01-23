58161e6f7da4273afd33318ac004a5e253415ba6
Sean Heffernan <seanmheff@gmail.com>
2017-01-23 19:48:25 +0000

    Improve syntax

--- lib/jitsi_scraper.ex
+++ lib/jitsi_scraper.ex
@@ -1,61 +1,44 @@
 defmodule JitsiScraper do
   use Timex
 
+  @url "http://lists.jitsi.org/pipermail/users/"
+  @regex ~r/From (.*) at (.*)  (.*)\nFrom: (.*) at (.*) \(.*\)\nDate.*\nSubject.*\n(.*\n.*\n)?Message-ID.*\n\n/
+
   def scrape do
-    url = "http://lists.jitsi.org/pipermail/users/"
-    body = HTTPoison.get!(url).body
-
-    # Get the links to download on this page
-    hrefs = Floki.attribute(body, "tr a", "href")
-
-    # Remove links we dont care about
-    gzipped_archives_paths = Enum.filter hrefs, fn(url) ->
-      #String.match?(url, ~r/\.*?.txt.gz/)
-      String.match?(url, ~r/2017-.*?.txt.gz/)
-    end
-
-    # GET gzip'd archives
-    gzipped_archives = Enum.map gzipped_archives_paths, fn(path) ->
-      HTTPoison.get!(url <> path).body
-    end
-
-    # Uncompress all the archives and return
-    Enum.map gzipped_archives, fn(archive) ->
-      :zlib.gunzip(archive)
-    end
+    HTTPoison.get!(@url).body
+    |> Floki.attribute("tr a", "href")
+    |> Enum.filter(fn(url) -> String.match?(url, ~r/2017-.*?.txt.gz/) end)
+    |> Enum.map(fn(path) -> HTTPoison.get!(@url <> path).body end)
+    |> Enum.map(fn(archive) -> :zlib.gunzip(archive) end)
   end
 
   def parse(archives) do
-    regex = ~r/From (.*) at (.*)  (.*)\nFrom: (.*) at (.*) \(.*\)\nDate.*\nSubject.*\n(.*\n.*\n)?Message-ID.*\n\n/
     Enum.map archives, fn(archive) ->
-      # Split the messages before at the email headers
-      [ _ | messages ] = String.split(archive, regex, include_captures: true)
-
-      # Chunk the headers and messages into lists and maybe update the DB
-      Enum.chunk(messages, 2) |> Enum.map(fn(group) -> map_group(group) end)
+      String.split(archive, @regex, include_captures: true, trim: true) # split at the headers
+      |> Enum.chunk(2) # combine headers and body
+      |> Enum.map(fn(group) -> map_group(group) end)
     end
   end
 
   defp map_group([ headers | body ]) do
-    # Split up the headers into their individual lines
-    # Ignore the first one. It's useless to us.
-    headers = tl(String.split(headers, "\n", trim: true)) |> Enum.map(fn(header) ->
-      String.split(header, ":", parts: 2)
-    end)
+    String.split(headers, "\n", trim: true)
+    |> tl # the first header is useless, discard it
+    |> Enum.map(fn(header) -> String.split(header, ":", parts: 2) end)
+    |> Enum.reduce(%{}, fn(header, acc) -> reduce_headers(header, body, acc) end)
+  end
 
-    Enum.reduce headers, %{}, fn(header, acc) ->
-      [key, value] = header
-      acc = case key do
-        "From" -> acc
-          |> Map.put(:name, parse_name(value))
-          |> Map.put(:from, parse_email(value))
-        "Date" -> Map.put(acc, :date, parse_date(value))
-        "Subject" -> Map.put(acc, :subject, String.trim(value))
-        "Message-ID" -> Map.put(acc, :message_id, String.trim(value))
-        _ -> acc
-      end
-      Map.put(acc, :body, body)
+  defp reduce_headers(header, body, acc) do
+    [key, value] = header
+    acc = case key do
+      "From" -> acc
+        |> Map.put(:name, parse_name(value))
+        |> Map.put(:from, parse_email(value))
+      "Date" -> Map.put(acc, :date, parse_date(value))
+      "Subject" -> Map.put(acc, :subject, String.trim(value))
+      "Message-ID" -> Map.put(acc, :message_id, String.trim(value))
+      _ -> acc
     end
+    Map.put(acc, :body, body)
   end
 
   defp parse_name(name) do
