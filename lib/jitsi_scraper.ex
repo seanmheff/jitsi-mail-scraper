594f2fd9c5db1f83205922a78872ec41c6f201ad
Sean Heffernan <seanmheff@gmail.com>
2017-01-24 14:18:46 +0000

    Make more robust

--- lib/jitsi_scraper.ex
+++ lib/jitsi_scraper.ex
@@ -2,39 +2,55 @@ defmodule JitsiScraper do
   use Timex
 
   @url "http://lists.jitsi.org/pipermail/users/"
-  @regex ~r/From (.*) at (.*)  (.*)\nFrom: (.*) at (.*) \(.*\)\nDate.*\nSubject.*\n(.*\n.*\n)?Message-ID.*\n\n/
+  @regex ~r/From (.*) at (.*)  (.*)\nFrom: (.*) at (.*) \(.*\)\nDate.*\n(Subject.*?\n(.*\n)*?)?(In-Reply-To:.*?\n)?(References:.*?\n(.*\n)*?)?Message-ID:.*\n\n/
+
+  def perform do
+    scrape() |> parse()
+  end
 
   def scrape do
     HTTPoison.get!(@url).body
     |> Floki.attribute("tr a", "href")
-    |> Enum.filter(fn(url) -> String.match?(url, ~r/2017-.*?.txt.gz/) end)
+    |> Enum.filter(fn(url) -> String.match?(url, ~r/2016-Nov.*?.txt.gz/) end)
     |> Enum.map(fn(path) -> HTTPoison.get!(@url <> path).body end)
     |> Enum.map(fn(archive) -> :zlib.gunzip(archive) end)
   end
 
   def parse(archives) do
-    Enum.map archives, fn(archive) ->
+    data = Enum.map archives, fn(archive) ->
       String.split(archive, @regex, include_captures: true, trim: true) # split at the headers
-      |> Enum.chunk(2) # combine headers and body
-      |> Enum.map(fn(group) -> map_group(group) end)
+      |> Enum.chunk(2) # combine headers and body for each message
+      |> Enum.map(fn(message) -> map_message(message) end)
+    end
+    Enum.reduce(data, [], fn(archive, acc) -> archive ++ acc end)
+  end
+
+  def map_message([ headers, body|_ ]) do
+    headers
+    |> String.split("\n", trim: true) # split the headers at each newline
+    |> tl # ignore the first header
+    |> Enum.reduce([], fn(item, acc) -> reduce_header_item(item, acc) end) # handle multi-line headers
+    |> Enum.map(fn(header) -> String.split(header, ":", parts: 2) end)
+    |> Enum.reduce(%{}, fn(header, acc) -> reduce_message(header, body, acc) end)
+  end
+
+  defp reduce_header_item(item, acc) do
+    if String.starts_with? item, ["\t", " "] do # if a line starts with a " " it belongs to the previous header
+      List.replace_at(acc, -1, List.last(acc) <> item)
+    else
+      List.insert_at(acc, -1, item)
     end
   end
 
-  defp map_group([ headers | body ]) do
-    String.split(headers, "\n", trim: true)
-    |> tl # the first header is useless, discard it
-    |> Enum.map(fn(header) -> String.split(header, ":", parts: 2) end)
-    |> Enum.reduce(%{}, fn(header, acc) -> reduce_headers(header, body, acc) end)
-  end
-
-  defp reduce_headers(header, body, acc) do
-    [key, value] = header
+  defp reduce_message([ key, value|_ ], body, acc) do
     acc = case key do
       "From" -> acc
         |> Map.put(:name, parse_name(value))
         |> Map.put(:from, parse_email(value))
       "Date" -> Map.put(acc, :date, parse_date(value))
       "Subject" -> Map.put(acc, :subject, String.trim(value))
+      "In-Reply-To" -> Map.put(acc, :in_reply_to, String.trim(value))
+      "References" -> Map.put(acc, :references, parse_references(value))
       "Message-ID" -> Map.put(acc, :message_id, String.trim(value))
       _ -> acc
     end
@@ -51,6 +67,14 @@ defmodule JitsiScraper do
   end
 
   defp parse_date(date) do
-    Timex.parse!(date, " %a, %e %b %Y %H:%M:%S %z", :strftime)
+    if Regex.match?(~r/^ \w{3}, \d+ \w{3} \d{4} \d{2}:\d{2}:\d{2} [+-]\d{4}$/, date) do
+      Timex.parse!(date, " %a, %e %b %Y %H:%M:%S %z", :strftime)
+    else
+      Timex.parse!(date, " %a, %e %b %Y %H:%M:%S %z (%Z)", :strftime)
+    end
+  end
+
+  defp parse_references(references) do
+    String.split(references, " ", trim: true)
   end
 end
--- test/jitsi_scraper_test.exs
+++ test/jitsi_scraper_test.exs
@@ -2,7 +2,35 @@ defmodule JitsiScraperTest do
   use ExUnit.Case
   doctest JitsiScraper
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  basic = File.read!("./test/fixtures/basic.txt")
+
+  describe "JitsiScraper.parser_header/1" do
+    test "it disgards the first 'From' header" do
+      response = JitsiScraper.parser_header basic
+    end
+
+    test "it parses the second 'From' header" do
+    end
+
+    test "it parses the 'Date' header" do
+    end
+
+    test "it parses the 'Subject' header" do
+    end
+
+    test "it parses the 'Subject' header when it spans multiple lines" do
+    end
+
+    test "it can parse the optional 'In-Reply-To' header" do
+    end
+
+    test "it can parse the optional 'References' header" do
+    end
+
+    test "it can parse the optional 'References' header when it spans multiple lines" do
+    end
+
+    test "it parses the 'Message-ID' header" do
+    end
   end
 end
